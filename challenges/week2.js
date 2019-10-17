044779fa2f2d49fc2c4889939433a04821e0afd2
Ben Holt <ben@wearemap.co.uk>
2019-10-17 19:49:14 +0100

    finished week 2

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,53 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  for (let key in sandwich) {
+    if (key === "fillings") {
+      return sandwich[key];
+    }
+  }
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  for (let key in person) {
+    if (person[key] === "Manchester") {
+      return true;
+    }
+  }
+  return false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let busValue = people / 40;
+  let busValueRound = Math.ceil(busValue);
+  return busValueRound
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  //   var newArr = [];
+  //   for (i=0; i < arr.length; i++)
+  //   var nSheep = arr.includes("sheep")
+  //  if (nSheep === true) {
+  //   newArr.push(arr[i])
+  //  }
+  //  return newArr.length
+  // }
 }
+
+
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  for (let key in person) {
+    const kValue = person[key]
+    if (kValue === "Manchester") {
+    } else if (kValue.startsWith("M")) {
+      return true;
+    }
+    return false;
+  }
 }
 
 module.exports = {
